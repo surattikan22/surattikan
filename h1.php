2926f6631d211031a583d417bd476bc6f9502cd4
surattikan22 <24519@srithatpit.ac.th>
2025-03-04 12:02:38 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTMLเบื้องต้น</title>
+</head>
+<body>
+    <p>------การแสดงข้อความแบบหัวข้อ------</p>
+    <div>
+        <h1>HTMLเบื้องต้น(h1)</h1>
+    </div>
+    <div>
+        <h2>HTMLเบื้องต้น(h1)</h2>
+        <h3>HTMLเบื้องต้น(h1)</h3>
+        <h4>HTMLเบื้องต้น(h1)</h4>
+        <h5>HTMLเบื้องต้น(h1)</h5>
+        <h6>HTMLเบื้องต้น(h1)</h6>
+    </div>
+    <p>------การแสดงตัวหนา ตัวเอียง การขีดเส้นใต้ข้อความ------</p>
+    <div>
+        <b>สุรัตน์ติกานต์ เรืองศาสตร์</b><br>
+        <i>สุรัตน์ติกานต์ เรืองศาสตร์</i><br>
+        <u>สุรัตน์ติกานต์ เรืองศาสตร์</u><br>
+    </div>
+    <p><b------การ Link------</b></p>
+    <div>
+
+        <a href="http://www.google.co.th">Google</a><br>
+    </div>    
+</body>
+</html>
